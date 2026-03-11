730bfdf763af31a3e6e7601261b3b4d88465cc7f
tusharborate46 <boratetushar46@gmail.com>
2026-03-11 22:31:09 +0530

    final project

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ fetch("http://localhost:5000/achievements")
 .then(res => res.json())
 .then(data => {
 
-//main portal body
+//main portal body 
 let table = document.getElementById("tablebody");
-
+let total = 0;
+let academic = 0;
+let sports = 0;
+let cultural = 0;
 data.forEach(item => {
 
 let row = document.createElement("tr");
@@ -18,14 +21,21 @@ row.innerHTML = `
 <td>${item.title}</td>
 <td>${item.category}</td>
 <td>${item.description}</td>
-<td>${item.file_upload || "-"}</td>
+<td>${item.file_path || "-"}</td>
 <td>${item.status || "Approved"}</td>
 `;
 
 table.appendChild(row);
+total++;
 
+if(item.category === "Academic") academic++;
+if(item.category === "Sports") sports++;
+if(item.category === "Cultural") cultural++;
 });
-
+document.getElementById("totalCount").innerText = total;
+document.getElementById("academicCount").innerText = academic;
+document.getElementById("sportsCount").innerText = sports;
+document.getElementById("culturalCount").innerText = cultural;
 });
 
 }
@@ -50,16 +60,45 @@ row.innerHTML = `
 <td>${item.title}</td>
 <td>${item.category}</td>
 <td>${item.description}</td>
-<td>${item.file_upload || "-"}</td>
-<td>${item.status || "Approved"}</td>
-<td>${item.verification || "Approved"}</td>
+<td>${item.file_path || "-"}</td>
+<td ">${item.status || "Pending"}</td>
+<td>
+<button class="approve" onclick="approveAchievement(${item.id})">Approve</button>
+<button class="reject" onclick="rejectAchievement(${item.id})">Reject</button>
+</td>
 
 `;
 
 table.appendChild(row);
 
+
 });
 
+});
+
+}
+function approveAchievement(id){
+
+fetch(`http://localhost:5000/approve/${id}`,{
+method:"PUT"
+})
+.then(res=>res.text())
+.then(msg=>{
+alert(msg);
+location.reload(); // refresh table
+});
+
+}
+
+function rejectAchievement(id){
+
+fetch(`http://localhost:5000/reject/${id}`,{
+method:"PUT"
+})
+.then(res=>res.text())
+.then(msg=>{
+alert(msg);
+location.reload();
 });
 
 }
@@ -95,4 +134,45 @@ fetch("http://localhost:5000/submit",{
 });
 
 });
+}
+
+
+
+//Login sccript
+if(document.getElementById("loginForm")){
+
+document.getElementById("loginForm").addEventListener("submit",function(e){
+
+e.preventDefault();
+
+let data = {
+username: document.getElementById("username").value,
+password: document.getElementById("password").value
+};
+
+fetch("http://localhost:5000/admin/login",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body: JSON.stringify(data)
+})
+.then(res=>res.json())
+.then(result=>{
+
+if(result.success){
+alert("Login successful");
+
+localStorage.setItem("adminLoggedIn",true);
+
+window.location.href="admin.html";
+}
+else{
+alert("Invalid login");
+}
+
+});
+
+});
+
 }
